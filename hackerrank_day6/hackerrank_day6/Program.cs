c40b760277bd7b0ea63a749fa8bbd22a9146599d
Mert Ellek <mertellek@hotmail.com>
2022-02-05 17:27:19 +0300

    hackerrank_day_6

---
+++ hackerrank_day6/hackerrank_day6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace hackerrank_day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string S = Console.ReadLine();
+                Cevir(S);
+            }
+        }
+
+        static void Cevir(string S)
+        {
+            string tek = "";
+            string cift = "";
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    cift = cift + S[i];
+                }
+                else
+                {
+                    tek = tek + S[i];
+                }
+            }
+
+            Console.WriteLine(cift + " " + tek);
+
+            Console.ReadKey();
+        }
+    }
+}

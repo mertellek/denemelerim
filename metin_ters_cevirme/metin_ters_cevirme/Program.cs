bce45af93bc7e6564b78f3567fb7b459d3ecac05
Mert Ellek <mertellek@hotmail.com>
2022-02-05 17:26:56 +0300

    metin_ters_cevirme

---
+++ metin_ters_cevirme/metin_ters_cevirme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace metin_ters_cevirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metin Giriniz: ");
+            string metin = Console.ReadLine();
+
+            Console.WriteLine("Metnin ters hali: ");
+
+            for (int i = metin.Length-1; i >=0; i--)
+            {
+                Console.WriteLine(metin[i]);
+            }
+
+            Console.ReadKey();
+       }
+    }
+}

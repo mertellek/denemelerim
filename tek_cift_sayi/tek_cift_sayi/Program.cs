dab8cd9e93a90e5a622a6b71cbf2a6e5cce1ca4f
Mert Ellek <mertellek@hotmail.com>
2022-02-05 17:26:42 +0300

    tek_cift_sayi

---
+++ tek_cift_sayi/tek_cift_sayi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace tek_cift_sayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N Sayısı giriniz: ");
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            bool weirdCheck = false;
+
+            if (N > 20)
+            {
+                weirdCheck = false;
+            }
+            else if (N >= 6 && N <= 20)
+            {
+                weirdCheck = true;
+            }
+            else if (N >= 2 && N <= 5 )
+            {
+                weirdCheck = false;
+            }
+            else if (N % 2 == 1)
+            {
+                weirdCheck = true;
+            }
+            else { Console.WriteLine("???"); }
+        
+               
+            
+            if (weirdCheck == true) { Console.WriteLine("Weird"); }
+            else { Console.WriteLine("Not Weird"); }
+
+            Console.ReadKey();
+        
+        }
+    }
+}

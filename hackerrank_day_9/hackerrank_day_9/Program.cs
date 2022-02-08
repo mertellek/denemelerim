76b12332cc6bc55aafb007b6fda9fa0842f8b44e
Mert Ellek <mertellek@hotmail.com>
2022-02-08 16:05:08 +0300

    hackerrank_day_9

---
+++ hackerrank_day_9/hackerrank_day_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hackerrank_day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            factorial(n);
+
+
+            Console.WriteLine(factorial(n));
+            Console.ReadKey();
+        }
+        public static int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+    }
+}

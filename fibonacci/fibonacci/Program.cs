e4c774de1cd3f34fabd404a58f5632ae1c9e967e
Mert Ellek <mertellek@hotmail.com>
2022-02-08 16:44:32 +0300

    fibonacci_sayi_dizisi

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayi+1; i++)
+            {
+                Console.WriteLine(i+". Fibonacci sayısı: "+ fib(i));
+            }
+            Console.ReadKey();
+        }
+
+        public static int fib(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fib(n - 1) + fib(n - 2);
+            }
+        }
+    }
+}

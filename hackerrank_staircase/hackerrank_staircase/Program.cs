de9a388f807a11472f4eea9355aec5a624fe4f38
Mert Ellek <mertellek@hotmail.com>
2022-02-09 15:56:19 +0300

    hackerrank_staircase

---
+++ hackerrank_staircase/hackerrank_staircase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hackerrank_staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int space = n-1; space > i; space--)
+                {
+                    Console.Write(" ");
+                }
+                for (int hash = 0; hash <= i; hash++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }                                   
+        }
+    }
+}

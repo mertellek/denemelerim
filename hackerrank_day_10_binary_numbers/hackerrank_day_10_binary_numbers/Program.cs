47246f495cf8f6fdfddb021f4a2590e8e0cc0ea0
Mert Ellek <mertellek@hotmail.com>
2022-02-09 15:34:19 +0300

    hackerrank_day_10_binary_numbers

---
+++ hackerrank_day_10_binary_numbers/hackerrank_day_10_binary_numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hackerrank_day_10_binary_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0, sayac = 0;
+            while (n>0)
+            {
+                
+                if (n%2==1)
+                {
+                    sayac ++;
+                    toplam = Math.Max(sayac, toplam);
+                }
+                else
+                {
+                    sayac = 0;
+                }
+                n /= 2;
+            }
+            Console.WriteLine(toplam);
+            Console.ReadKey();
+        }
+    }
+}

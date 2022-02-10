861406f9dc473d33dec80b8bcc96bc6ff5dd2076
Mert Ellek <mertellek@hotmail.com>
2022-02-10 21:11:48 +0300

    coderbyte_counting_minutes

---
+++ coderbyte_deneme_1/coderbyte_deneme_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace coderbyte_deneme_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //12 saatlik sistemde örnekteki gibi yazılan iki saat arasındaki dakika farkını veren program.
+            //Örnek: 12:00am-11:00am
+            
+            string str =Console.ReadLine();
+            
+            string[] tire = str.Split('-');
+
+            DateTime tire0 = DateTime.Parse(tire[0]);
+
+            DateTime tire1 = DateTime.Parse(tire[1]);
+
+            if (tire1 < tire0)
+            {
+                TimeSpan ts = tire0 - tire1;
+                Console.WriteLine(1440 - ts.TotalMinutes);
+            }
+            else
+            {
+                TimeSpan ts = tire1 - tire0;
+                Console.WriteLine(1440 - ts.TotalMinutes);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
